08204e81067d336232744c232d632973eea2f8e0
OglodkovVadim <73477542+OglodkovVadim@users.noreply.github.com>
2023-03-30 19:54:15 +0300

    Create binarySearch.cpp

---
+++ binarySearch.cpp
@@ -0,0 +1,19 @@
+template<typename Iterator, typename T>
+T binarySearch(Iterator begin, Iterator end, T val)
+{
+    end--; // increase becouse end point to end elem + 1
+    while (std::distance(begin, end) >= 0) {
+        auto mid = begin;
+        std::advance(mid, (std::distance(begin, end)) / 2);
+      
+        if (*mid == val)
+            return *mid;
+        else if (std::distance(begin, end) == 0) // closed corner case when (end = begin) && (mid != vol)
+            return 0;
+        else if (*mid < val)
+            std::advance(begin, std::distance(begin, mid) + 1);
+        else
+            std::advance(end, -1*(std::distance(mid, end)));
+    }
+    return 0;
+}
